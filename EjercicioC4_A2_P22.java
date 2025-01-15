997f56150da88cf1198cb9fae517dd1e8e6da81c
SanlopezreUnal <sanlopezre@unal.edu.co>
2025-01-14 21:35:50 -0500

    Add files via upload

---
+++ EjercicioC4_A2_P22.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejercicioc4_a2_p22;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class EjercicioC4_A2_P22 {
+
+    public static void main(String[] args) {
+        VentanaPrincipal ventana = new VentanaPrincipal();
+        ventana.setVisible(true);
+        
+    }
+}
---
+++ Empleado.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.ejercicioc4_a2_p22;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Empleado {
+    
+    public static double calcular_salario_meunsual(double salario_basico_hora, double numero_horas_trabajadas){
+        double salario_mensual;
+
+        salario_mensual = salario_basico_hora*numero_horas_trabajadas;
+        
+        if (salario_mensual>450000){
+            return salario_mensual;
+        }
+        else{
+            return salario_mensual;
+        }
+    }
+    
+    public static String set_Nombre(String nombre){
+        
+        return nombre;
+    }
+    
+    public static String limpiar_Campos(){
+        return "";
+    }
+}
+    
+
